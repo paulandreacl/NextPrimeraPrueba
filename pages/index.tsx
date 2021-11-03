a8c272d26bbc10c08845b86e84e3a9d5918da362
paulandreacl <paula150995@hotmail.com>
2021-11-03 00:16:39 -0500

    agregar despliegue en dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Home: NextPage = () => {
   return (
    <div>
-     <p>Hola mundo</p>
+     <p>Hola mundo -dev</p>
    <Link href='/admin/users'>
      <a>Ir a admin usuarios</a>
    </Link>
