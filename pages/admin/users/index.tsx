96469db99631ba00e80b46538e871d24599d0105
paulandreacl <paula150995@hotmail.com>
2021-11-03 00:02:10 -0500

    creación de rutas y paginas

---
+++ pages/admin/users/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+           usuarios pagina 
+        </div>
+    )
+}
+
+export default index
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,11 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if(req.method == 'GET'){
+    res.status(200).json({ name: 'hice un get' })
+  }
+  if(req.method == 'POST'){
+    res.status(200).json({ name: 'hice un post' })
+  }
+  
 }
